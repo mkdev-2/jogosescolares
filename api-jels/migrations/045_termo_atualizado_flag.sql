248915c6b947f049a6e60b922d25a678d1ff72dc
Itallo Eduardo <mvcas95@gmail.com>
2026-04-22 15:49:26 -0300

    ajustes nas migrations

--- api-jels/migrations/045_termo_atualizado_flag.sql
+++
@@ -1,4 +0,0 @@
--- Adiciona flag para indicar que o termo foi atualizado após edição pós-prazo
-
-ALTER TABLE escola_edicao_modalidades
-ADD COLUMN IF NOT EXISTS termo_atualizado BOOLEAN NOT NULL DEFAULT FALSE;
