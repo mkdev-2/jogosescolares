3135baeb0d7300f7795a062442bb52a4a3149436
Itallo Eduardo <mvcas95@gmail.com>
2026-05-11 10:07:42 -0300

    refactor: remover tabelas e migrações obsoletas de notícias e categorias

--- api-jels/migrations/030_expand_campeonato_fase_enum.sql
+++
@@ -1,4 +0,0 @@
--- Migration 030: amplia fases do mata-mata para suportar chaves até 64 vagas.
-
-ALTER TYPE campeonato_fase_enum ADD VALUE IF NOT EXISTS 'TRINTA_E_DOIS_AVOS';
-ALTER TYPE campeonato_fase_enum ADD VALUE IF NOT EXISTS 'DEZESSEIS_AVOS';
